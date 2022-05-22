0af90c7c0263cc6a9dd764772b3b79d8798b4d14
ganesh behera <naruto.ganesh007@gmail.com>
2022-05-23 04:20:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Reactmobileos from './component/Ractmobileos'
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Reactmobileos/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Ractmobileos.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+function Ractmobileos() {
+  return (
+    <div>
+        <h1>Mobile Oporating System</h1>
+        <div>
+        <ul >
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>Iphone</li>
+            <li>Windoes Phone</li>
+        </ul>
+        </div>
+        <div>
+            <h1>Mobail Manufacturers</h1>
+            <ul>
+                <li>Samsung</li>
+                <li>Htc</li>
+                <li>Maicromax</li>
+                <li>Apple</li>
+            </ul>
+        </div>
+        
+    </div>
+   
+  )
+}
+
+export default Ractmobileos
